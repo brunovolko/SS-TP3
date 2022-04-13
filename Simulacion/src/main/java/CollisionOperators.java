6c3e853e8751bfcd3f988e7476f3c7a15d050bc9
Bruno Volcovinsky <brunovolko@gmail.com>
2022-04-13 13:33:14 -0300

    Calculate velocity after particles collision

--- Simulacion/src/main/java/CollisionOperators.java
+++ Simulacion/src/main/java/CollisionOperators.java
@@ -4,7 +4,16 @@ public class CollisionOperators {
     public static void particleToParticle(List<Particle> particles, Particle particle1, Particle particle2) {
         int idx1 = particles.indexOf(particle1), idx2 = particles.indexOf(particle2);
 
-//       //TODO calcular velocidades post choque elastico
+        double sigma = particle1.getRadius() + particle2.getRadius();
+        double deltaVdeltaR = (particle2.getVx() - particle1.getVx())*(particle2.getX()-particle1.getX()) + (particle2.getVy() - particle1.getVy())*(particle2.getY()-particle1.getY());
+        double J = (2*particle1.getMass()*particle2.getMass()*deltaVdeltaR) / (sigma*(particle1.getMass()+particle2.getMass()));
+        double Jx = (J * (particle2.getX() - particle1.getX()))/sigma;
+        double Jy = (J * (particle2.getY() - particle1.getY()))/sigma;
+        
+        particle1.setVx(particle1.getVx() + Jx/particle1.getMass());
+        particle1.setVy(particle1.getVy() + Jy/particle1.getMass());
+        particle2.setVx(particle2.getVx() - Jx/particle2.getMass());
+        particle2.setVy(particle2.getVy() - Jy/particle2.getMass());
 
         particles.set(idx1, particle1);
         particles.set(idx2, particle2);
