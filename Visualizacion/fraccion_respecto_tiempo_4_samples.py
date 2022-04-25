ef5f0d39086fc32957ac870fb2b8668280570a50
Bruno Volcovinsky <brunovolko@gmail.com>
2022-04-24 23:38:38 -0300

    Fix visualization

--- Visualizacion/fraccion_respecto_tiempo_4_samples.py
+++ Visualizacion/fraccion_respecto_tiempo_4_samples.py
@@ -11,10 +11,15 @@ plt.plot(df_100_02['t'], df_100_02['fraccion'], label='N=100, D=0.02', zorder=1,
 plt.plot(df_300_02['t'], df_300_02['fraccion'], label='N=300, D=0.02', zorder=1, color='orange')
 plt.plot(df_100_045['t'], df_100_045['fraccion'], label='N=100, D=0.045', zorder=1, color='green')
 plt.plot(df_300_045['t'], df_300_045['fraccion'], label='N=300, D=0.045', zorder=1, color='red')
-plt.scatter(df_100_02.where(df_100_02['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_100_02.where(df_100_02['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#111163', marker='o', s=50, zorder=2)
-plt.scatter(df_300_02.where(df_300_02['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_300_02.where(df_300_02['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#9a6b15', marker='o', s=50, zorder=2)
-plt.scatter(df_100_045.where(df_100_045['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_100_045.where(df_100_045['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#0b450b', marker='o', s=50, zorder=2)
-plt.scatter(df_300_045.where(df_300_045['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_300_045.where(df_300_045['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#890f0f', marker='o', s=50, zorder=2)
+try:
+    plt.scatter(df_100_02.where(df_100_02['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_100_02.where(df_100_02['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#111163', marker='o', s=50, zorder=2)
+    plt.scatter(df_300_02.where(df_300_02['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_300_02.where(df_300_02['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#9a6b15', marker='o', s=50, zorder=2)
+    plt.scatter(df_100_045.where(df_100_045['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_100_045.where(df_100_045['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#0b450b', marker='o', s=50, zorder=2)
+    plt.scatter(df_300_045.where(df_300_045['fraccion'] <= 0.5).dropna().iloc[0]['t'], df_300_045.where(df_300_045['fraccion'] <= 0.5).dropna().iloc[0]['fraccion'], color='#890f0f', marker='o', s=50, zorder=2)
+except:
+    pass
+
+
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Fraccion de particulas a izquierda')
 plt.yticks(np.append(np.arange(min_value, 1, 0.03), [1]))
